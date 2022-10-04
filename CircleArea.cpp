8feb9bb39b7f09f6ecc6debaf34879706fd66e05
Ns1501 <76951338+Ns1501@users.noreply.github.com>
2022-10-05 00:31:54 +0530

    Add files via upload

---
+++ CircleArea.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    float r;
+    cout<<"Enter Radius :";
+    cin>>r;
+    cout<<"Area of circle is:"<<3.14*r*r;
+   }
