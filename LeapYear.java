eb6350f21d0224180702d2749284c448d0c84615
Nishant Mohanty <73649715+Nishant50@users.noreply.github.com>
2022-10-10 12:50:45 +0530

    Added Leap year program in java

---
+++ LeapYear.java
@@ -0,0 +1,32 @@
+// Java program to check
+// for a leap year
+	
+class Test
+{
+	static boolean checkYear(int year)
+	{
+		// If a year is multiple of 400,
+		// then it is a leap year
+		if (year % 400 == 0)
+			return true;
+	
+		// Else If a year is multiple of 100,
+		// then it is not a leap year
+		if (year % 100 == 0)
+			return false;
+	
+		// Else If a year is multiple of 4,
+		// then it is a leap year
+		if (year % 4 == 0)
+			return true;
+		return false;
+	}
+		
+	// Driver method
+	public static void main(String[] args)
+	{
+		int year = 2000;
+		System.out.println( checkYear(2000)? "Leap Year" :
+						"Not a Leap Year" );
+	}
+}
