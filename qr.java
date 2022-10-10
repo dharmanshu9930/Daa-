1696eae466072fa011176675c0babfa445dcf01d
arora2suman <115494833+arora2suman@users.noreply.github.com>
2022-10-10 23:17:05 +0530

    Create qr.java

---
+++ qr.java
@@ -0,0 +1,13 @@
+public class QuotientRemainder {
+
+  public static void main(String[] args) {
+
+    int dividend = 25, divisor = 4;
+
+    int quotient = dividend / divisor;
+    int remainder = dividend % divisor;
+
+    System.out.println("Quotient = " + quotient);
+    System.out.println("Remainder = " + remainder);
+  }
+}
