6b08be5f9acc546ea198937c9635b6c6ed6d59d3
sushantrana04 <115220343+sushantrana04@users.noreply.github.com>
2022-10-11 11:24:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome | PG Life</title>
+
+    <?php
+    include "includes/head_links.php";
+    ?>
+    <link href="css/home.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php
+    include "includes/header.php";
+    ?>
+
+    <div class="banner-container">
+        <h2 class="white pb-3">Happiness per Square Foot</h2>
+
+        <form id="search-form" action="property_list.php" method="GET">
+            <div class="input-group city-search">
+                <input type="text" class="form-control input-city" id='city' name='city' placeholder="Enter your city to search for PGs" />
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-secondary">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div class="page-container">
+        <h1 class="city-heading">
+            Major Cities
+        </h1>
+        <div class="row">
+            <div class="city-card-container col-md">
+                <a href="property_list.php?city=Delhi">
+                    <div class="city-card rounded-circle">
+                        <img src="img/delhi.png" class="city-img" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="city-card-container col-md">
+                <a href="property_list.php?city=Mumbai">
+                    <div class="city-card rounded-circle">
+                        <img src="img/mumbai.png" class="city-img" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="city-card-container col-md">
+                <a href="property_list.php?city=Bengaluru">
+                    <div class="city-card rounded-circle">
+                        <img src="img/bangalore.png" class="city-img" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="city-card-container col-md">
+                <a href="property_list.php?city=Hyderabad">
+                    <div class="city-card rounded-circle">
+                        <img src="img/hyderabad.png" class="city-img" />
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include "includes/signup_modal.php";
+    include "includes/login_modal.php";
+    include "includes/footer.php";
+    ?>
+
+</body>
+
+</html>
