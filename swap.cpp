868434b66758f7c80fddc1caf10c4fbf3142d514
shourya169 <116200024+shourya169@users.noreply.github.com>
2022-10-19 23:31:00 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=3,b=5,temp;
+    cout<<"before swapping"<<a<<"and"<<b;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"after swapping"<<a<<"and"<<b;
+    return 0;
+
+}
